29f946c420bc45a821559063e733baf6b2e9b776
Justin Pope <jpope986@gmail.com>
2019-08-22 09:10:38 -0400

    Added help files for FBB; synching

---
+++ fbb/ru/help_lang.php
@@ -0,0 +1,11 @@
+<?php
+$lang['order_q1'] = "What do the different order statuses mean?";
+$lang['Updatable'] = "Updatable";
+$lang['order_a1'] = "All line items in the order were fulfilled.";
+$lang['order_a2'] = "Some of the line items were fulfilled with the remaining to be fulfilled when inventory or funds are added.";
+$lang['order_a3'] = "The order is in the process of being picked and packed.";
+$lang['order_a4'] = "The order is waiting to be packed.";
+$lang['order_a5'] = "The order is waiting to be processed by the system. This is the default state.";
+$lang['order_a6'] = "The order is being held by the customer and won't be processed.";
+$lang['order_a7'] = "There is a problem with the shipping address and it must be corrected by the customer.";
+$lang['order_a8'] = "There is insufficient inventory for one or more line items, or the customer must add more funds to their account balance.";
